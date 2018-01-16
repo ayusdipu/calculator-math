ac474969c7c68d37517ea5d754bf23f551be44bf
albert <ayusdipu@gmail.com>
2018-01-16 13:24:46 +0700

    adding Godly Level

--- Calculator.js
+++ Calculator.js
@@ -1,10 +1,12 @@
 // Calculator
 
 
+// Level Zero
 // Input Variable
 var A = 4//prompt("Please enter a number",5);
 var B = 4//prompt("Please enter a number",5);
 
+// Level One
 // Calculator functions
 function add(a,b) {
     return a+b;
@@ -24,4 +26,37 @@ function modulo(a,b) {
 
 console.log(add(A,B));
 
+// Level Two
 console.log(add(substract(A,B), divide(A,B)));
+
+
+// Hahaha Level Three - Stat Master
+var regression = require('regression');
+
+
+const result = regression.linear([[0, 1], [32, 67], [12, 79]]);
+const gradient = result.equation[0];
+const yIntercept = result.equation[1];
+
+
+const data2 = [[0,1],[32, 67],[12, 79]];
+const result2 = regression.polynomial(data2, { order: 3 });
+
+console.log(result);
+console.log(gradient);
+console.log(yIntercept);
+console.log(result2);
+
+
+// Godly Level - Became Ash Ketchum
+const calculator = require('pokemon-stat-calculator');
+
+let charizardBaseStats = [78, 104, 78, 159, 115, 100];
+let fullIVs = [31, 31, 31, 31, 31, 31];
+let zeroIVs = [0, 0, 0, 0, 0, 0];
+let emptyEVs = [0, 0, 0, 0, 0, 0];
+let fullEVs = [6, 0, 0, 252, 0, 252];
+let nature = [1, 1, 1, 1, 1];
+ 
+var pokemon = calculator.calAllStats(fullIVs, charizardBaseStats, emptyEVs, 50, nature);
+console.log("This is Charizard Status " + pokemon);
